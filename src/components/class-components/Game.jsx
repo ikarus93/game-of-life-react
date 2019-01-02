b842f354fc40981155d73ee40ccc6337c5da509b
Farid Wilhelm Zimmermann <williamgazilliontrillion@gmail.com>
2019-01-02 22:34:30 +0000

    Improved main game component and added seperate Speed and Size controls

--- src/components/class-components/Game.jsx
+++ src/components/class-components/Game.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-class Game extends Component {
+class Game extends React.Component {
   constructor() {
     super();
+    this.state = {
+      gameSpeed: 1,
+      sizeTable: [10, 50, 100],
+      currentSize: 10
+    }
+    this.changeGameSpeed = this.changeGameSpeed.bind(this);
+    this.changeSize = this.changeSize.bind(this);
+    
+  }
+  
+  changeSize(event) {
+    const size = parseInt(event.target.value);
+    if (this.state.sizeTable[size] !== this.state.currentSize) {
+      this.setState({
+        currentSize: this.state.sizeTable[size]
+      })
+    }
+  }
+  
+  changeGameSpeed(event) {
+    this.setState({
+      gameSpeed: parseInt(event.target.value)
+    })
   }
 
   render() {
     return (
       <React.Fragment>
-        <CanvasContainer xDimension={10} yDimension={10} />
+        <SizeControl changeSize={this.changeSize}/>
+        <CanvasContainer xDimension={this.state.currentSize} yDimension={this.state.currentSize} speed={this.state.gameSpeed}/>
+        <SpeedControl changeGameSpeed={this.changeGameSpeed} />
       </React.Fragment>
     );
   }
